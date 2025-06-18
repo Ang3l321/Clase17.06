c0e01de055b65b46532910d4cde23dfa400dcf30
Ang3l321 <168379428+Ang3l321@users.noreply.github.com>
2025-06-17 21:17:34 -0500

    Add files via upload

---
+++ Clase.17.06/Archivo.11.py
@@ -0,0 +1,19 @@
+""" 
+CICLOS ANIDADOS 
+Sirve para contabilizar el  tiempo 
+Ejemplo 2
+
+Contar dias y semanas  con
+ciclos anidados
+
+"""
+
+"""for semana in range (1, 21):
+    for dias in range (1,6):
+        print(f'*', end='')
+    print()"""
+
+for minutos in range (1, 2):
+    for segundos in range (1, 61):
+        print(f'{minutos}   {segundos}')
+        
